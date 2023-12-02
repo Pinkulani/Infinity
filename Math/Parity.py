7bd77a982663dbe5fec70013345fb85d39884ef4
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-12-02 13:08:08 +0000

    Adding parity checking

---
+++ Math/Parity.py
@@ -0,0 +1,11 @@
+def Even(Input):
+    if (Input % 2) == 0:
+        return True
+    else:
+        return False
+    
+def Odd(Input):
+    if (Input % 2) == 0:
+        return True
+    else:
+        return False
