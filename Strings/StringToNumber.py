19b1a4e7b981ca7f4aebbdc44ebde5563086bc0d
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-12-02 13:13:29 +0000

    Adding string to number converter

---
+++ Strings/StringToNumber.py
@@ -0,0 +1,25 @@
+def StringToNumber(Input):
+    Number = Input
+    match Number:
+        case "0":
+            Number = 0
+        case "1":
+            Number = 1
+        case "2":
+            Number = 2
+        case "3":
+            Number = 3
+        case "4":
+            Number = 4
+        case "5":
+            Number = 5
+        case "6":
+            Number = 6
+        case "7":
+            Number = 7
+        case "8":
+            Number = 8
+        case "9":
+            Number = 9
+    
+    return Number
